cb3b36eed36096eb0e692bbe6c640b4dce38c6de
jennyteng <jenny.teng@hsc.utah.edu>
2020-03-09 17:10:18 -0400

    1st checkin for this sample app

---
+++ src/sample-apps/web-offline-db/README.md
@@ -0,0 +1,14 @@
+
+## Installation 
+I assume that you already have **Davic** installed. If not, please install Davic first. 
+In addition to Davic, you will also need **Gorilla/mux** framework. 
+Executing the python script of this directory should help installing it: 
+```
+python myinstall.py
+```
+
+## Run
+In command-line:  
+```
+go run main.go
+```
---
+++ src/sample-apps/web-offline-db/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	// "strings"
+	"net/http"
+	// "encoding/json"
+	"github.com/gorilla/mux"
+	// "io/ioutil"
+	// "wfchiang/davic"
+)
+
+const (
+	KEY_STORE_HTTP_REQUEST  = "http-reqt"
+	KEY_STORE_HTTP_RESPONSE = "http-resp"
+)
+
+// ====
+// Utils 
+// ====
+
+// ==== 
+// Recovery function 
+// ====
+func recoverFromPanic (http_resp http.ResponseWriter, id_service string) {
+	if r := recover(); r != nil {
+		err_message := fmt.Sprintf("%v", r)
+		log.Println(fmt.Sprintf("[%s] %s", id_service, err_message))
+		fmt.Fprintf(http_resp, err_message)
+	}
+}
+
+// ====
+// Handlers 
+// ====
+func homepageHandler (http_resp http.ResponseWriter, http_reqt *http.Request) {
+	log.Println("Homepage is Hit!")
+	fmt.Fprintf(http_resp, "Web-Offline-DB is Here!")
+}
+
+// ====
+// Main
+// ====
+func main () {
+	log.Println("Init File Server...")
+	file_server := http.FileServer(http.Dir("./static"))
+
+	log.Println("Starting Web-Offline-DB...")
+	mux_router := mux.NewRouter()
+
+	mux_router.PathPrefix("/pages/").Handler(http.StripPrefix("/pages/", file_server))
+	mux_router.HandleFunc("/", homepageHandler)
+
+	http.Handle("/", mux_router)
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
